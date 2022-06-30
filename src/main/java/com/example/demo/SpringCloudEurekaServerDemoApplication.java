f6373bc8200f53c7294427d381773af0e8861658
oldcheese <okistheengineer@gmail.com>
2022-06-30 15:26:48 +0800

    add health check api

--- src/main/java/com/example/demo/SpringCloudEurekaServerDemoApplication.java
+++ src/main/java/com/example/demo/SpringCloudEurekaServerDemoApplication.java
@@ -3,13 +3,22 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaServer
 @SpringBootApplication
+@RestController
 public class SpringCloudEurekaServerDemoApplication {
 
+	@GetMapping("health")
+	public String health() {
+		return "ok";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudEurekaServerDemoApplication.class, args);
+		
 	}
 
 }
